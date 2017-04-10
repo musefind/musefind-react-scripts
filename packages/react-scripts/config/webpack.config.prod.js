f03e453cdc0dbcf72fbdeb10b74552602dbf7755
scottdomes <scottdomes@gmail.com>
2017-04-10 16:27:16 -0700

    Fixed problem with enzyme in node modules and conditional requires

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -94,7 +94,10 @@ module.exports = {
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web'
+      'react-native': 'react-native-web',
+      'react/addons': 'react-addons-test-utils',
+      'react/lib/ReactContext': 'react-addons-test-utils',
+      'react/lib/ExecutionEnvironment': 'react-addons-test-utils'
     }
   },
   // @remove-on-eject-begin
